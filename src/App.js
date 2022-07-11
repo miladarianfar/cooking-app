6fc31daac63ad9169b897cda658a7c9fcc8230f4
Milad Arianfar <miladarianfar8471@gmail.com>
2022-07-11 16:36:37 +0430

    add navbar component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Create from './pages/create/Create';
 import Home from './pages/home/Home';
 import Recipe from './pages/recipe/Recipe';
 import Search from './pages/search/Search';
-
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar />
         <Switch>
           <Route exact path="/">
             <Home />
