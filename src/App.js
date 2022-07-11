4cc2c76233aa09257f622b3992e33469fd6cccb7
Milad Arianfar <miladarianfar8471@gmail.com>
2022-07-11 16:29:43 +0430

    create router in project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Create from './pages/create/Create';
+import Home from './pages/home/Home';
+import Recipe from './pages/recipe/Recipe';
+import Search from './pages/search/Search';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/recipe">
+            <Recipe />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
