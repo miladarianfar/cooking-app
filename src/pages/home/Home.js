d7c0db71d2fbbe2cddd1a56f4361435d150fc6d4
Milad Arianfar <miladarianfar8471@gmail.com>
2022-07-12 18:14:38 +0430

    change react router version

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,17 @@
 import './Home.css';
+import { useFetch } from '../../hooks/useFetch'
 
 export default function Home() {
+
+    const {data: recipes, error, isPending} = useFetch("http://localhost:3000/recipes")
+
     return (
-        <div>
-            Home
+        <div className="home">
+            {error && <div className="error">{error.message}</div>}
+            {isPending && <div className="loader">is Loading...</div>}
+            {recipes && recipes.map(recipe => (
+                <h2 key={recipe.id}>{recipe.title}</h2>
+            ))}
         </div>
     )
 }
